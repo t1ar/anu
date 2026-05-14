f6bca3ba5b48a7a8df07bd3e6dcce7c9e345ca7f
HelicopKun <szplay69@gmail.com>
2026-05-14 16:37:07 +0700

    skill _inproduction

---
+++ battle_entity/script/in_battle.gd
@@ -0,0 +1,4 @@
+class_name in_battle
+#extends Battle_entity
+
+@export var skill_list: Array[Skill] = []
---
+++ skill/script/skill.gd
@@ -0,0 +1,6 @@
+class_name Skill
+extends Resource
+
+@export var skill_name: String = ""
+@export var affect: Array[Affect] = []
+@export var scene: PackedScene
