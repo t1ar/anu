75da88b8087df6a9d6c81c5cee18ea57744f21bd
Zyreee <153530579+KakPaj@users.noreply.github.com>
2026-06-01 22:07:20 +0700

    Add some bots and fix sound bug

--- views/pause_menu.py
+++ views/pause_menu.py
@@ -47,6 +47,7 @@ class PauseMenuView(arcade.View):
             from views.main_menu import MainMenuView
             menu_view = MainMenuView()
             self.window.show_view(menu_view)
+            arcade.stop_sound(self.game_view.music_player)
         elif key == arcade.key.R:
             self.window.show_view(self.game_view)
             self.game_view._setup()
